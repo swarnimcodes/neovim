0a51e69c600d5ba22be420348fb887828250efad
Swarnim B <swarnim14.9@hotmail.com>
2024-07-22 17:43:33 +0530

    Added few mini-plugins

---
+++ lua/ripmeapart/plugins/mini-git.lua
@@ -0,0 +1,21 @@
+return {
+  "echasnovski/mini-git",
+  version = "*",
+  main = "mini.git",
+  opts = {
+    -- General CLI execution
+    job = {
+      -- Path to Git executable
+      git_executable = "git",
+
+      -- Timeout (in ms) for each job before force quit
+      timeout = 30000,
+    },
+
+    -- Options for `:Git` command
+    command = {
+      -- Default split direction
+      split = "auto",
+    },
+  },
+}
